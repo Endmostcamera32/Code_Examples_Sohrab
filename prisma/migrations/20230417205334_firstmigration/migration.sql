7d425605a2314df2b3c9b89699ab586f7b70d9b6
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2023-04-17 14:01:37 -0700

    working on most recent changes

--- prisma/migrations/20230417205334_firstmigration/migration.sql
+++ prisma/migrations/20230417205334_firstmigration/migration.sql
@@ -1,62 +1,62 @@
 -- CreateTable
 CREATE TABLE "Post" (
-    "postId" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "language" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "totalComments" INTEGER DEFAULT 0,
-    "totalLikes" INTEGER DEFAULT 0,
+    "postId" STRING NOT NULL,
+    "code" STRING NOT NULL,
+    "language" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "totalComments" INT4 DEFAULT 0,
+    "totalLikes" INT4 DEFAULT 0,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
+    "userId" STRING NOT NULL,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("postId")
 );
 
 -- CreateTable
 CREATE TABLE "Comment" (
-    "commentId" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
+    "commentId" STRING NOT NULL,
+    "content" STRING NOT NULL,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "userId" STRING NOT NULL,
+    "postId" STRING NOT NULL,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("commentId")
 );
 
 -- CreateTable
 CREATE TABLE "like" (
-    "likeId" TEXT NOT NULL,
+    "likeId" STRING NOT NULL,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
-    "userId" TEXT NOT NULL,
-    "postId" TEXT NOT NULL,
+    "userId" STRING NOT NULL,
+    "postId" STRING NOT NULL,
 
     CONSTRAINT "like_pkey" PRIMARY KEY ("likeId")
 );
 
 -- CreateTable
 CREATE TABLE "Account" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "provider" TEXT NOT NULL,
-    "providerAccountId" TEXT NOT NULL,
-    "refresh_token" TEXT,
-    "access_token" TEXT,
-    "expires_at" INTEGER,
-    "token_type" TEXT,
-    "scope" TEXT,
-    "id_token" TEXT,
-    "session_state" TEXT,
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "type" STRING NOT NULL,
+    "provider" STRING NOT NULL,
+    "providerAccountId" STRING NOT NULL,
+    "refresh_token" STRING,
+    "access_token" STRING,
+    "expires_at" INT4,
+    "token_type" STRING,
+    "scope" STRING,
+    "id_token" STRING,
+    "session_state" STRING,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Session" (
-    "id" TEXT NOT NULL,
-    "sessionToken" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
+    "id" STRING NOT NULL,
+    "sessionToken" STRING NOT NULL,
+    "userId" STRING NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
@@ -64,19 +64,19 @@ CREATE TABLE "Session" (
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "name" TEXT,
-    "email" TEXT,
+    "id" STRING NOT NULL,
+    "name" STRING,
+    "email" STRING,
     "emailVerified" TIMESTAMP(3),
-    "image" TEXT,
+    "image" STRING,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "VerificationToken" (
-    "identifier" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
+    "identifier" STRING NOT NULL,
+    "token" STRING NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL
 );
 
